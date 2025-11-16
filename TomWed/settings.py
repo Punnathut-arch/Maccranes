4a83278c805a62675db19305bd394ca53070c9b9
MAC <punnathut.chai@gmail.com>
2025-11-16 14:27:33 +0700

    แก้ STATIC_ROOT และอัปเดต base.html

--- TomWed/settings.py
+++ TomWed/settings.py
@@ -122,6 +122,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/Allweb/'
 STATICFILES_DIRS = [BASE_DIR /'Allweb']
 
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
